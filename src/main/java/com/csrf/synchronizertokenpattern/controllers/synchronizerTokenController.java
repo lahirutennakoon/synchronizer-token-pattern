c43f2ca9a351146faa48b26ec23d375c79c9373a
Lahiru Tennakoon <lahiru@speralabs.com>
2018-08-08 16:40:53 +0530

    created controller

---
+++ src/main/java/com/csrf/synchronizertokenpattern/controllers/synchronizerTokenController.java
@@ -0,0 +1,8 @@
+package com.csrf.synchronizertokenpattern.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class synchronizerTokenController
+{
+}
