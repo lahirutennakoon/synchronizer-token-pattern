71ddee237e94d20dfed489e0dc494155063b2f20
Lahiru Tennakoon <lahiru@speralabs.com>
2018-08-08 17:46:44 +0530

    jquery form submit

--- src/main/java/com/csrf/synchronizertokenpattern/controllers/synchronizerTokenController.java
+++ src/main/java/com/csrf/synchronizertokenpattern/controllers/synchronizerTokenController.java
@@ -1,8 +1,14 @@
 package com.csrf.synchronizertokenpattern.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class synchronizerTokenController
 {
+    @PostMapping("/login")
+    public boolean loginUser()
+    {
+        return  true;
+    }
 }
